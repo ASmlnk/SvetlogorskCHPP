b9386257e9131e938e6ef2f0415ab9cb861ed28d
user <sdelanss@gmail.com>
2024-08-11 10:14:33 +0300

    Commit House

---
+++ app/src/main/java/com/example/svetlogorskchpp/presentation/shift_schedule_calendar_add_notes/ShiftScheduleAddNotesFragment.kt
@@ -0,0 +1,8 @@
+package com.example.svetlogorskchpp.presentation.shift_schedule_calendar_add_notes
+
+import androidx.fragment.app.Fragment
+
+class ShiftScheduleAddNotesFragment: Fragment() {
+
+
+}
