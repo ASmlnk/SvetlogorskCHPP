2c28b0ba3d7faaf0707aa0761a8f386a7ed04656
user <sdelanss@gmail.com>
2024-09-10 18:46:33 +0300

    Commit House

--- app/src/main/java/com/example/svetlogorskchpp/__di/NotificationModule.kt
+++ app/src/main/java/com/example/svetlogorskchpp/__di/NotificationModule.kt
@@ -4,6 +4,7 @@ import android.app.NotificationManager
 import android.content.Context
 import com.example.svetlogorskchpp.__data.repository.calendarNoteTag.CalendarNoteTagRepository
 import com.example.svetlogorskchpp.__data.repository.note.NoteRepository
+import com.example.svetlogorskchpp.__domain.usecases.calendarDateUseCases.CalendarDateUseCases
 import com.example.svetlogorskchpp.__domain.usecases.calendarNoteNotificationUseCases.CalendarNoteNotificationUseCases
 import com.example.svetlogorskchpp.__domain.usecases.calendarNoteNotificationUseCases.CalendarNoteNotificationUseCasesImpl
 import dagger.Module
@@ -27,7 +28,7 @@ object NotificationModule {
     fun provideCalendarNoteNotificationUseCases(
         calendarNoteTagRepository: CalendarNoteTagRepository,
         noteRepository: NoteRepository,
-        calendarDateUseCases: CalendarNoteNotificationUseCases,
+        calendarDateUseCases: CalendarDateUseCases,
     ): CalendarNoteNotificationUseCases {
         return CalendarNoteNotificationUseCasesImpl(
             calendarNoteTagRepository,
