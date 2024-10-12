0b88d7a70d6e8583b03033bc6fa195e5902ef29d
DelaNx <sdelanss@gmail.com>
2024-10-12 07:23:39 +0300

    commit job

--- app/src/main/java/com/example/svetlogorskchpp/__presentation/dialog/request_work_filter/view_model/RequestWorkFilterViewModel.kt
+++ app/src/main/java/com/example/svetlogorskchpp/__presentation/dialog/request_work_filter/view_model/RequestWorkFilterViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.svetlogorskchpp.__presentation.dialog.request_work_filter.view_model
 
 import androidx.lifecycle.ViewModel
+import com.example.svetlogorskchpp.__domain.interactor.shift_schedule.note_list.ShiftScheduleNoteListInteractor
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
+@HiltViewModel
 class RequestWorkFilterViewModel @Inject constructor(
     private val shiftScheduleNoteListInteractor: ShiftScheduleNoteListInteractor
 ): ViewModel() {
