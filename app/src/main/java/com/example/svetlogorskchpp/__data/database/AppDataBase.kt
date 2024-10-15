3fa0997299998e2b4270529621f48caa57693780
user <sdelanss@gmail.com>
2024-10-15 19:43:37 +0300

    Commit House

--- app/src/main/java/com/example/svetlogorskchpp/__data/database/AppDataBase.kt
+++ app/src/main/java/com/example/svetlogorskchpp/__data/database/AppDataBase.kt
@@ -64,7 +64,7 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
 
 val MIGRATION_4_5 = object : Migration(4, 5) {
     override fun migrate(db: SupportSQLiteDatabase) {
-        db.execSQL("ALTER TABLE request_work ADD COLUMN permission TEXT NOT NULL")
+        db.execSQL("ALTER TABLE request_work ADD COLUMN permission TEXT NOT NULL DEFAULT 'default_value'")
     }
 }
 
